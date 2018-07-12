07c6203358eaf8d72879cd7e265d1088ef933116
Fernando Espinosa <fernandoespinosa@fernandoespinosa.org>
2018-07-11 20:02:43 -0400

    chore: Update `angular.json` to clear `newProjectRoot`
    chore: `ng g library orders --prefix=orders`

---
+++ orders/src/lib/orders.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'orders-orders',
+  template: `
+    <p>
+      orders works!
+    </p>
+  `,
+  styles: []
+})
+export class OrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ orders/src/lib/orders.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { OrdersComponent } from './orders.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [OrdersComponent],
+  exports: [OrdersComponent]
+})
+export class OrdersModule { }
